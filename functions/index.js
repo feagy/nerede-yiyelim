e23f3d41a356bbc1b4817b0bd236c561e1aa862d
feagy <ferhat1903dnz@gmail.com>
2025-11-04 22:09:36 +0300

    searchPlaces güncellendi , getPhoto eklendi

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,7 @@ const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 exports.searchPlaces = functions.https.onRequest((req, res) => {
     cors(req, res, async () => {
         try {
-            const { lat, lng} = req.query;
+            const { lat, lng, radius = 50000 } = req.query;
 
             if (!lat || !lng) {
                 return res.status(400).json({ error: "lat ve lng zorunlu" });
@@ -17,7 +17,7 @@ exports.searchPlaces = functions.https.onRequest((req, res) => {
                 return res.status(500).json({ error: "API_KEY eksik" });
             }
 
-            const results = await places.getNearbyPlaces(GOOGLE_API_KEY, lat, lng);
+            const results = await places.getNearbyPlaces(GOOGLE_API_KEY, lat, lng, radius);
 
             return res.status(200).json({ places: results });
         } catch (error) {
@@ -28,5 +28,15 @@ exports.searchPlaces = functions.https.onRequest((req, res) => {
             });
         }
     });
+});
+
+exports.getPhoto = functions.https.onRequest(async (req, res) => {
+    const { photoName, maxWidth = 400 } = req.query;
+    if (!photoName) {
+        return res.status(400).json({ error: "photoName zorunlu" });
+    }
+    const response = await places.getPlacePhoto(GOOGLE_API_KEY, photoName, maxWidth);
+    res.set("Content-Type", "image/jpeg");
+    res.send(response);
 });
          
--- functions/places.js
+++ functions/places.js
@@ -1,8 +1,9 @@
 const axios = require("axios");
 module.exports.places = {
     getNearbyPlaces,
+    getPlacePhoto,
 };
-async function getNearbyPlaces(API_KEY, lat, lng, radius = 50000, maxCount = 20) {
+async function getNearbyPlaces(API_KEY, lat, lng, radius, maxCount = 20) {
     const url = "https://places.googleapis.com/v1/places:searchNearby";
     const body = {
         languageCode: "tr",
@@ -21,20 +22,52 @@ async function getNearbyPlaces(API_KEY, lat, lng, radius = 50000, maxCount = 20)
     const headers = {
       "Content-Type": "application/json",
       "X-Goog-Api-Key": API_KEY,
-      "X-Goog-FieldMask":
-      "places.displayName,places.formattedAddress,places.location,places.rating,places.primaryTypeDisplayName",
+      "X-Goog-FieldMask": `places.id,
+                           places.displayName,
+                           places.internationalPhoneNumber,
+                           places.formattedAddress,
+                           places.regularOpeningHours,
+                           places.location,
+                           places.rating,
+                           places.userRatingCount,
+                           places.reviews,
+                           places.primaryTypeDisplayName,
+                           places.googleMapsUri,
+                           places.googleMapsLinks,
+                           places.photos`.replace(/\s+/g, ""),
     };
 
+
+    // places.googleMapsLinks,places.generativeSummary
     const response = await axios.post(url, body, { headers });
     const places = response.data.places?.map((p) => ({
+        id: p.id,
         name: p.displayName?.text || "Ad Yok",
+        phone: p.internationalPhoneNumber || "Telefon Yok",
         address: p.formattedAddress || "Adres Yok",
+        openingHours: p.regularOpeningHours || null,
         rating: p.rating || null,
+        userRatingCount: p.userRatingCount || null,
+        reviews: p.reviews?.map((r) => ({
+          name: r.authorAttribution?.displayName || "Yazar Yok",
+          text: r.originalText?.text || "Yorum Yok",
+          rating: r.rating || 0,
+        })) ||null,
         type: p.primaryTypeDisplayName?.text || "",
         lat: p.location?.latitude,
         lng: p.location?.longitude,
+        googleMapsUri: p.googleMapsUri || null,
+        googleMapsLinks: p.googleMapsLinks || null,
+        photos: p.photos?.map((ph) => ({
+          photoName: ph.name,
+        })) || null,
     }));
     return places || [];
+}
 
+async function getPlacePhoto(API_KEY, photoName, maxWidth) {
+    const url = `https://places.googleapis.com/v1/${photoName}/media?maxWidthPx=${maxWidth}&key=${API_KEY}`;
+    const response = await axios.get(url, { responseType: "arraybuffer" });
+    return response.data;
 }
 
