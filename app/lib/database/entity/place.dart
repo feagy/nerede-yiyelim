2c7358e1edb1a7cb884d9ff196e90bb920b869b0
feagy <ferhat1903dnz@gmail.com>
2026-01-05 00:16:33 +0300

    placeservices eklendi ve endpointlerde güncellemeler

--- app/lib/database/entity/place.dart
+++ app/lib/database/entity/place.dart
@@ -5,9 +5,55 @@ class Place {
   @primaryKey
   final String id;
 
+  final double distance;
   final String placeName;
+  final String? phone;
+  final String? address;
 
-  final String placeLocation;
+  final double lat;
+  final double lng;
 
-  Place(this.id, this.placeName, this.placeLocation);
+  final double? googleRating;
+  final int? googleRatingCount;
+
+  final String? type;
+
+  final String? photoName;
+  final String? googleReviewsJson;
+  final String? openingHoursJson;
+
+
+  Place({
+    required this.id,
+    required this.distance,
+    required this.placeName,
+    this.phone,
+    this.address,
+    required this.lat,
+    required this.lng,
+    this.googleRating,
+    this.googleRatingCount,
+    this.type,
+    this.photoName,
+    this.googleReviewsJson,
+    this.openingHoursJson,
+  });
+
+  factory Place.fromJson(Map<String, dynamic> json) {
+    return Place(
+      id: json['id'] as String,
+      distance: (json['distance'] as num?)?.toDouble() ?? 0.0,
+      placeName: json['name'] as String? ?? 'Ad yok',
+      phone: json['phone'] as String?,
+      address: (json['address'] as String?) ?? 'Adress yok',
+      lat: (json['lat'] as num).toDouble(),
+      lng: (json['lng'] as num).toDouble(),
+      googleRating: (json['googleRating'] as num?)?.toDouble() ?? 0.0,
+      googleRatingCount: json['googleRatingCount'] as int?,
+      type: json['type'] as String?,
+      photoName: json['photoName'] as String?,
+      googleReviewsJson: json['reviews'] as String?,
+      openingHoursJson: json['openingHours'] as String?,
+    );
+  }
 }
