3fdb0dab779cda624c06b596ccea9710c3ea13d3
DoritosLover1 <ihsandmrci10@gmail.com>
2026-01-10 03:04:36 +0300

    Added a function to login account and save account information to db

--- app/lib/functions/additionalfunc.dart
+++ app/lib/functions/additionalfunc.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
+import 'package:app/database/services/localdbservice.dart';
+import 'package:app/services/authservice.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:app/database/entity/account.dart';
-import 'package:app/database/dto/account_dto.dart';
-
+/*
 Future<void> _pickAndSavePhoto(AccountDto dao, Account account) async {
   final picker = ImagePicker();
   final pickedFile = await picker.pickImage(source: ImageSource.gallery);
@@ -26,5 +28,32 @@ Future<void> _pickAndSavePhoto(AccountDto dao, Account account) async {
 
     // DB’de güncelle
     await dao.updateAccount(updatedAccount);
+  }
+}
+*/
+Future<bool> signInAndSaveAccount({ required String email, required String password }) async {
+  try {
+    final db = await LocalServices.getDatabase();
+    await AuthService().signIn(email: email, password: password);
+    
+    final doc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(AuthService().currentUser!.uid)
+        .get();
+    
+    if (doc.exists) {
+      await db.accountDao.insertAccount(
+        Account(
+          id: AuthService().currentUser?.uid ?? "",
+          email: email,
+          userName: doc['nickname'] ?? "",
+          password: password,
+        ),
+      );
+      return true;
+    }
+    return false;
+  } catch (e) {
+    return false;
   }
 }
